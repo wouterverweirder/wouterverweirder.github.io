c4d3e266c7715942d27feca1fe0922bb9c37816c
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-07-06 11:29:08 +0200

    fix fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,17 @@
 // custom typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
+import '@fontsource/poppins/300.css'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/700.css'
+import '@fontsource/fira-sans/100.css'
+import '@fontsource/fira-sans/300.css'
+import '@fontsource/fira-sans/400.css'
+import '@fontsource/fira-sans/500.css'
+import '@fontsource/fira-sans/600.css'
+import '@fontsource/fira-sans/700.css'
+
 require('prismjs/themes/prism.css')
 require('katex/dist/katex.min.css')
