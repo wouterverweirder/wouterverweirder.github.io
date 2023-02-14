393539e1c8b356a7342cbc0e9bf6e125fc54b9e9
Wouter Verweirder <wouter.verweirder@gmail.com>
2023-02-14 13:13:10 +0100

    added picture element and astro-image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.aboutme.be',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.aboutme.be',
+  integrations: [mdx(), sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
